2eb8b753de038fa4df943234f52b2c3f2660f4c8
juanbarrios <juanmanuel@barrios.cl>
2015-09-07 20:11:34 -0300

    print compiled version

--- metricknn/metricknn_cli/src/metricknn/metricknn_cli/metricknn_cli.cpp
+++ metricknn/metricknn_cli/src/metricknn/metricknn_cli/metricknn_cli.cpp
@@ -15,6 +15,8 @@ int main_search(std::vector<std::string> &args);
 int main_kmeans(std::vector<std::string> &args);
 int main_pca(std::vector<std::string> &args);
 
+#define STR(x) #x
+
 static void print_version() {
 	std::cout << "This file is part of MetricKnn. http://metricknn.org/" << std::endl;
 	std::cout << "MetricKnn is made available under the terms of the BSD 2-Clause License." << std::endl;
@@ -22,7 +24,7 @@ static void print_version() {
 	std::cout << "There is NO WARRANTY, to the extent permitted by law." << std::endl;
 	std::cout << "" << std::endl;
 #ifdef VERSION_NAME
-	std::cout << "Compiled version: " << VERSION_NAME << std::endl;
+	std::cout << "Compiled version: " << STR(VERSION_NAME) << std::endl;
 #endif
 }
 
